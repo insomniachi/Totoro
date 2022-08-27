cd5fb0c955f17803ee30d7d87ecd56938b92d9c3
Athul Raj <athulrajts@gmail.com>
2022-08-27 14:40:41 +0530

    use animixplay featured anime in discover page

--- AnimDL.UI.Core/ViewModels/DiscoverViewModel.cs
+++ AnimDL.UI.Core/ViewModels/DiscoverViewModel.cs
@@ -1,28 +1,26 @@
-﻿using MalApi;
+﻿using System.Text.Json.Serialization;
+using MalApi;
 using MalApi.Interfaces;
 
 namespace AnimDL.UI.Core.ViewModels;
 
 public class DiscoverViewModel : NavigatableViewModel, IHaveState
 {
-    private readonly IMalClient _client;
-
-    public DiscoverViewModel(IMalClient client)
+    public DiscoverViewModel()
     {
-        _client = client;
 
         Observable
             .Timer(TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(10))
             .ObserveOn(RxApp.MainThreadScheduler)
             .Subscribe(_ =>
             {
-                if (TopAiring.Count == 0)
+                if (Featured.Count == 0)
                 {
                     SelectedIndex = 0;
                     return;
                 }
 
-                if (SelectedIndex == TopAiring.Count - 1)
+                if (SelectedIndex == Featured.Count - 1)
                 {
                     SelectedIndex = 0;
                     return;
@@ -32,33 +30,45 @@ public class DiscoverViewModel : NavigatableViewModel, IHaveState
             });
     }
 
-    [Reactive] public ObservableCollection<Anime> TopAiring { get; set; } = new();
+    [Reactive] public ObservableCollection<FeaturedAnime> Featured { get; set; }
     [Reactive] public int SelectedIndex { get; set; }
 
     public void RestoreState(IState state)
     {
+        Featured = state.GetValue<ObservableCollection<FeaturedAnime>>(nameof(Featured));
     }
 
     public async Task SetInitialState()
     {
-        var result = await _client.Anime()
-                                  .Top(AnimeRankingType.Airing)
-                                  .WithLimit(10)
-                                  .Find();
-
-        result.Data.ForEach(async x =>
-        {
-            TopAiring.Add(await _client.Anime()
-                .WithId(x.Anime.Id)
-                .WithField(x => x.Background)
-                .WithField(x => x.Pictures)
-                .WithField(x => x.Synopsis)
-                .WithField(x => x.Genres)
-                .Find());
-        });
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.54");
+        var stream = await client.GetStreamAsync("https://animixplay.to/assets/s/featured.json");
+        Featured = await JsonSerializer.DeserializeAsync<ObservableCollection<FeaturedAnime>>(stream);
     }
 
     public void StoreState(IState state)
     {
+        state.AddOrUpdate(Featured);
     }
+}
+
+public class FeaturedAnime
+{
+    public string Id => Url?.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Take(1).FirstOrDefault();
+    public string[] GenresArray => Genres?.Split(",");
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    [JsonPropertyName("img")]
+    public string Image { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string Genres { get; set; }
+
+    [JsonPropertyName("desc")]
+    public string Description { get; set; }
 }
