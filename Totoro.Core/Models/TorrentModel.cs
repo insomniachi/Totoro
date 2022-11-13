d658941e76c88a9d9e467fb1ed06696a8fec6489
Athul Raj <athulrajts@gmail.com>
2022-11-13 17:05:37 +0530

    add download speed

--- Totoro.Core/Models/TorrentModel.cs
+++ Totoro.Core/Models/TorrentModel.cs
@@ -1,10 +1,12 @@
-﻿using MonoTorrent.Client;
+﻿using Humanizer;
+using MonoTorrent.Client;
 
 namespace Totoro.Core.Models
 {
     public class TorrentModel : ReactiveObject
     {
         private readonly ObservableAsPropertyHelper<double> _progress;
+        private readonly ObservableAsPropertyHelper<string> _downloadSpeed;
 
         public TorrentModel(TorrentManager torrentManager)
         {
@@ -16,6 +18,11 @@ namespace Totoro.Core.Models
                 .Select(_ => Math.Round(torrentManager.Progress, 2))
                 .ToProperty(this, x => x.Progress, out _progress, scheduler: RxApp.MainThreadScheduler, initialValue: 0);
 
+            Observable
+                .Timer(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2))
+                .Select(_ => $"{torrentManager.Monitor.DownloadSpeed.Bytes().Humanize()}/s")
+                .ToProperty(this, x => x.DownloadSpeed, out _downloadSpeed, scheduler: RxApp.MainThreadScheduler, initialValue: "");
+
             this.WhenAnyValue(x => x.Progress)
                 .Where(p => p == 100)
                 .Subscribe(_ => IsCompleted = true);
@@ -24,6 +31,7 @@ namespace Totoro.Core.Models
 
         public string Name { get; set; }
         public double Progress => _progress.Value;
+        public string DownloadSpeed => _downloadSpeed.Value;
         [Reactive]public bool IsCompleted { get; set; }
         public TorrentManager Downloader { get; }
     }
