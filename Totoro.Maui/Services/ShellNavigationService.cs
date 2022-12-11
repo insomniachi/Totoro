3e8076fccb6ab7cf2cd7fe7550aa47a0a3fbb971
Athul Raj <athulrajts@gmail.com>
2022-12-11 11:27:03 +0530

    update to net 7.0

---
+++ Totoro.Maui/Services/ShellNavigationService.cs
@@ -0,0 +1,24 @@
+﻿using Totoro.Core.Contracts;
+
+namespace Totoro.Maui.Services
+{
+    internal class ShellNavigationService : INavigationService
+    {
+        public bool CanGoBack => throw new NotImplementedException();
+
+        public bool GoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NavigateTo(Type viewModelType, object viewModel = null, IReadOnlyDictionary<string, object> parameter = null, bool clearNavigation = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool INavigationService.NavigateTo<TViewModel>(TViewModel viewModel, IReadOnlyDictionary<string, object> parameter, bool clearNavigation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
