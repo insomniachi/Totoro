c7ced116d2d225e18b34ba8fe62cf9716f0e3104
Athul Raj <athulrajts@gmail.com>
2023-05-27 20:35:58 +0530

    remove language ext

--- Totoro.Plugins.Anime/Contracts/IAnimeStreamProvider.cs
+++ Totoro.Plugins.Anime/Contracts/IAnimeStreamProvider.cs
@@ -1,5 +1,4 @@
-﻿using LanguageExt.Common;
-using Totoro.Plugins.Anime.Models;
+﻿using Totoro.Plugins.Anime.Models;
 
 namespace Totoro.Plugins.Anime.Contracts;
 
@@ -13,28 +12,4 @@ public interface IMultiLanguageAnimeStreamProvider
 {
     public IAsyncEnumerable<VideoStreamsForEpisode> GetStreams(string url, Range episodeRange, StreamType streamType);
     public Task<int> GetNumberOfStreams(string url, StreamType streamType);
-}
-
-
-public static class StreamProviderExtensions
-{
-    public static async Task<Result<VideoStreamsForEpisode>> GetStream(this IAnimeStreamProvider provider, string url, int episode)
-    {
-        await foreach (var item in provider.GetStreams(url, episode..episode))
-        {
-            return item;
-        }
-
-        return new(new Exception("Episode not found"));
-    }
-
-    public static async Task<Result<VideoStreamsForEpisode>> GetStream(this IMultiLanguageAnimeStreamProvider provider, string url, int episode, StreamType streamType)
-    {
-        await foreach (var item in provider.GetStreams(url, episode..episode, streamType))
-        {
-            return item;
-        }
-
-        return new(new Exception("Episode not found"));
-    }
 }
