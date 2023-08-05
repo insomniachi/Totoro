9b199cbc0c39ea2536a0829ce0a9645b82e1eda6
Athul Raj <athulrajts@gmail.com>
2023-08-05 08:53:44 +0530

    workaround for community toolkit error

---
+++ Totoro.WinUI/UserControls/CTK.cs
@@ -0,0 +1,12 @@
+﻿namespace Totoro.WinUI.UserControls;
+
+public class Loading : CommunityToolkit.WinUI.UI.Controls.Loading { }
+public class ImageEx : CommunityToolkit.WinUI.UI.Controls.ImageEx { }
+public class WrapPanel : CommunityToolkit.WinUI.UI.Controls.WrapPanel { }
+public class AdaptiveGridView : CommunityToolkit.WinUI.UI.Controls.AdaptiveGridView { }
+public class Expander : CommunityToolkit.WinUI.UI.Controls.Expander { }
+public class Case : CommunityToolkit.WinUI.UI.Controls.Case { }
+public class DataGrid : CommunityToolkit.WinUI.UI.Controls.DataGrid { }
+public class DataGridTemplateColumn : CommunityToolkit.WinUI.UI.Controls.DataGridTemplateColumn { }
+public class DataGridTextColumn : CommunityToolkit.WinUI.UI.Controls.DataGridTextColumn { }
+public class UniformGrid : CommunityToolkit.WinUI.UI.Controls.UniformGrid { }
