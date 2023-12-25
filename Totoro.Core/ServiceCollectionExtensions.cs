c9de2e7c76e1bcec93d2c4b53f54bd363c56b475
Insomniachi <athulrajts@gmail.com>
2023-12-25 16:06:11 +0530

    use mica alt

--- Totoro.Core/ServiceCollectionExtensions.cs
+++ Totoro.Core/ServiceCollectionExtensions.cs
@@ -132,10 +132,10 @@ namespace Totoro.Core
             PluginFactory<AnimeProvider>.Instance.LoadPlugin(new Plugins.Anime.AllAnime.Plugin());
             PluginFactory<AnimeProvider>.Instance.LoadPlugin(new Plugins.Anime.YugenAnime.Plugin());
             PluginFactory<AnimeProvider>.Instance.LoadPlugin(new Plugins.Anime.GogoAnime.Plugin());
-            PluginFactory<AnimeProvider>.Instance.LoadPlugin(new Plugins.Anime.Marin.Plugin());
+            //PluginFactory<AnimeProvider>.Instance.LoadPlugin(new Plugins.Anime.Marin.Plugin());
             PluginFactory<AnimeProvider>.Instance.LoadPlugin(new Plugins.Anime.AnimeSaturn.Plugin());
             PluginFactory<AnimeProvider>.Instance.LoadPlugin(new Plugins.Anime.WitAnime.Plugin());
-            PluginFactory<AnimeProvider>.Instance.LoadPlugin(new Plugins.Anime.Aniwave.Plugin());
+            //PluginFactory<AnimeProvider>.Instance.LoadPlugin(new Plugins.Anime.Aniwave.Plugin());
 
             // manga
             PluginFactory<MangaProvider>.Instance.LoadPlugin(new Plugins.Manga.MangaDex.Plugin());
