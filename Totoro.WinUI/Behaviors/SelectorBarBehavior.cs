9892052e801066df8d71df14c71cd822596b097a
Insomniachi <athulrajts@gmail.com>
2024-03-02 11:36:00 +0530

    use selectorbar instead of segmented

---
+++ Totoro.WinUI/Behaviors/SelectorBarBehavior.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+using Totoro.Core.ViewModels;
+using ReactiveMarbles.ObservableEvents;
+
+namespace Totoro.WinUI.Behaviors;
+
+public class SelectorBarBehavior : Behavior<SelectorBar>
+{
+    public IEnumerable<PivotItemModel> ItemSource
+    {
+        get { return (IEnumerable<PivotItemModel>)GetValue(ItemSourceProperty); }
+        set { SetValue(ItemSourceProperty, value); }
+    }
+
+    public PivotItemModel SelectedItem
+    {
+        get { return (PivotItemModel)GetValue(SelectedItemProperty); }
+        set { SetValue(SelectedItemProperty, value); }
+    }
+
+    public static readonly DependencyProperty SelectedItemProperty =
+        DependencyProperty.Register("SelectedItem", typeof(PivotItemModel), typeof(SelectorBarBehavior), new PropertyMetadata(null));
+
+    public static readonly DependencyProperty ItemSourceProperty =
+        DependencyProperty.Register("ItemSource", typeof(IEnumerable<PivotItemModel>), typeof(SelectorBarBehavior), new PropertyMetadata(Enumerable.Empty<PivotItemModel>()));
+
+
+    protected override void OnAttached()
+    {
+        this.WhenAnyValue(x => x.ItemSource)
+            .Where(x => x is not null && x.Any() && AssociatedObject is not null)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe(values =>
+            {
+                AssociatedObject.Items.Clear();
+                foreach (var item in values.Where(x => x.Visible))
+                {
+                    AssociatedObject.Items.Add(new SelectorBarItem
+                    {
+                        Text = item.Header,
+                        FontSize = 20
+                    });
+                }
+
+                if(SelectedItem is null)
+                {
+                    AssociatedObject.SelectedItem = AssociatedObject.Items.FirstOrDefault();
+                }
+            });
+
+        this.WhenAnyValue(x => x.SelectedItem)
+            .Where(_ => AssociatedObject is not null)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe(item =>
+            {
+                if (AssociatedObject.Items.FirstOrDefault(x => x.Text == item.Header) is not { } uiItem)
+                {
+                    return;
+                }
+
+                AssociatedObject.SelectedItem = uiItem;
+            });
+
+        AssociatedObject
+            .Events()
+            .SelectionChanged
+            .Select(x => x.sender.SelectedItem?.Text)
+            .Where(text => !string.IsNullOrEmpty(text))
+            .Subscribe(header => SelectedItem = ItemSource.FirstOrDefault(x => x.Header == header));
+
+    }
+}
---
+++ Totoro.WinUI/Helpers/SelectionBarExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Totoro.WinUI.Helpers;
+
+public class SelectionBarExtensions
+{
+
+    public static IEnumerable<object> GetItemSource(DependencyObject obj)
+    {
+        return (IEnumerable<object>)obj.GetValue(ItemSourceProperty);
+    }
+
+    public static void SetItemSource(DependencyObject obj, IEnumerable<object> value)
+    {
+        obj.SetValue(ItemSourceProperty, value);
+    }
+
+    public static readonly DependencyProperty ItemSourceProperty =
+        DependencyProperty.RegisterAttached("ItemSource", typeof(IEnumerable<object>), typeof(SelectorBar), new PropertyMetadata(Enumerable.Empty<object>(), OnItemSourceChanged));
+
+    private static void OnItemSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var selectionBar = (SelectorBar)d;
+
+        if (e.NewValue is not IEnumerable<object> values || !values.Any())
+        {
+            return;
+        }
+
+        selectionBar.DispatcherQueue.TryEnqueue(() =>
+        {
+            selectionBar.Items.Clear();
+            foreach (var item in values)
+            {
+                selectionBar.Items.Add(new SelectorBarItem
+                {
+                    Text = item.ToString(),
+                    FontSize = 20
+                });
+            }
+        });
+    }
+}
